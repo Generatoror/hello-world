094f0004e4c550e9eebdaf0aec0bacc9001d90be
Generatoror <sebistian.mondo@gmail.com>
2017-04-01 17:56:39 -0600

    Update catManifest.js

--- catManifest.js
+++ catManifest.js
@@ -1,7 +1,7 @@
 G.DeclareManifest({
 name:'test mod manifest',
 updates:{
-	'Test mod*':'cookie.js',//we're updating any mod with a name that begins with "Example mod"
+	'Test mod*':'https://github.com/Generatoror/hello-world/blob/master/cookieLegacy.js',//we're updating any mod with a name that begins with "Example mod"
 	//you could declare other mod updates here if you wanted
 }
 });
