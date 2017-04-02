b2b26135387d3073e0857ca411860f902f235b18
Generatoror <sebistian.mondo@gmail.com>
2017-04-02 08:46:50 -0500

    Update cookieManifest.js

--- cookieManifest.js
+++ cookieManifest.js
@@ -1,6 +1,7 @@
 G.DeclareManifest({
 name:'test mod manifest',
 updates:{
+	'Example mod*':'mod.js',//we're updating any mod with a name that begins with "Example mod"
 	'Test mod*':'https://rawgit.com/Generatoror/hello-world/master/cookieLegacy.js',//we're updating any mod with a name that begins with "Example mod"
 	//you could declare other mod updates here if you wanted
 }
