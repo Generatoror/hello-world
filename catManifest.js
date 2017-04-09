48d83670bb8c4149d63dfa305eca4154e31d8b4c
Generatoror <sebistian.mondo@gmail.com>
2017-04-09 13:55:12 -0500

    Update catManifest.js

--- catManifest.js
+++ catManifest.js
@@ -2,7 +2,7 @@ G.DeclareManifest({
 name:'test mod manifest',
 updates:{
 	'Example mod*':'mod.js',//we're updating any mod with a name that begins with "Example mod"
-	'Test mod*':'https://rawgit.com/Generatoror/hello-world/master/cookieLegacy.js',//we're updating any mod with a name that begins with "Example mod"
-	//you could declare other mod updates here if you wanted
+	'Test mod*':'https://rawgit.com/Generatoror/hello-world/master/cookieLegacy.js',
+	'Thot mod*':'https://rawgit.com/torcado194/neverending-legacy-mods/master/thot.js'
 }
 });
